3101819fc899edd0b9645082646ea5f59f84a116
hamza0342 <63608271+hamza0342@users.noreply.github.com>
2020-09-05 20:59:52 +0500

    3rd commit

---
+++ hello-world/src/Approval.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Approval= props => 
+{
+    return (
+    <div>
+        <div> Hello {props.name}. Your id is {props.id} and your semester is {props.semester}</div>
+        
+    </div>
+    
+    );
+
+
+}
+
+export default Approval;
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Approval from './Approval';
+import './style.css';
+
 
 const App = () => {
-    return <div>Hello world</div>
+    return <div>
+        <div className="first">
+         <Approval name="Hamza Toor"
+          id= {3}
+          semester= {1}
+         
+         />
+         </div>
+         <div className="second">
+             Hello from COMSATS University Islamabad
+             </div>
+         <div className="third">
+             <Approval name="Ali"
+             id= {8789}
+             semester= {3}
+             />
+         </div>
+    </div>
+    
+       
+    
+
 }
 ReactDOM.render(<App />, document.getElementById('root'));
